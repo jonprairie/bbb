f2599b032d5ab5f1d02c629035a28ed79411acfa
Prairie <augustusseizure@gmail.com>
2016-10-24 10:52:35 -0400

    extended proj.py to handle extracting original filenames from local paths and qualifying them with the new project path

--- proj.py
+++ proj.py
@@ -7,11 +7,12 @@ import os
 class Proj:
     def __init__(self, name, host=None, user=None, pth=None, auto_load=False):
         self.name = name
-        if pth is not None and not os.path.isabs(pth):
-            pth = os.path.abspath(pth)
-        elif pth is None:
-            pth = os.path.abspath(name)
-        self.pth = pth
+
+        # orig_path is the original path passed into proj,
+        # pth is the qualified path to save all tracked, local files
+        self.orig_path = pth
+        self.pth = self.qual_path(pth)
+
         self.auto_load = auto_load
 
         self.user = user
@@ -19,7 +20,40 @@ class Proj:
 
         self.local_to_host = dict()
         self.host_to_local = dict()
-        if self.orig_path 
+
+    def move_proj(self, pth):
+        new_pth = self.qual_path(pth)
+        new_l_to_h = {
+            os.path.join(
+                new_pth,
+                self.extr_orig_file_name(l_fl)
+            ): h_fl
+            for l_fl, h_fl in self.local_to_host.items()
+        }
+        new_h_to_l = {
+            h_fl: l_fl for l_fl, h_fl in new_l_to_h.items()
+        }
+        self.local_to_host = new_l_to_h
+        self.host_to_local = new_h_to_l
+
+    def extr_orig_file_name(self, fl_pth):
+        if fl_pth.startswith(self.pth):
+            old_pth = os.path.join(self.pth, '')
+            orig_file = fl_pth[len(old_pth):]
+            return orig_file
+        else:
+            raise ValueError(
+                "%s isn't qualified by %s" % (fl_pth, self.pth)
+            )
+
+
+
+    def qual_path(self, pth):
+        if pth is not None and not os.path.isabs(pth):
+            pth = os.path.abspath(pth)
+        elif pth is None:
+            pth = os.path.abspath(self.name)
+        return pth   
 
     def add_tracker(self, host_fl=None, local_fl=None):
         """add tracked file"""
